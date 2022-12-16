338cb994eff2d457415e3a00e5945499a4fc2f8e
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-16 18:31:43 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -23,28 +23,7 @@ CUMA TEST DOANG
 
     # Help Message
     HELP = """
-1) Add me as **Admin** to a group.
-
-2) Add me to the particular chat as **Admin** where you want to force your users to join. It can be any group or channel, public or private.
-
-3) Use /fsub chat_id/username to make me functional. Use /id if you need chat id.
-Example : `/fsub -1001505616678` or `/forcesubscribe -1001375849192`
-
-4) [Optional] Use /settings to change settings!
-
-5) You are good to go. Leave the rest to me.
-
-✨ **Available Commands** ✨
-
-/fsub - See current force subscribe chat
-/fsub chat_id/username - Force users to join the particular chat
-/settings - Change Group Settings
-/id - Get the chat id of any group or channel
-/about - About The Bot
-/help - This Message
-/start - Start the Bot
-
-**Note** - You can also use `/forcesubscribe` instead of `/fsub`
+**Jika ada kendala atau ingin melaporkan link rusak bisa chat admin di [Channel NekoPoi](https://t.me/NekoPoiSupport).**
     """
 
     # About Message
