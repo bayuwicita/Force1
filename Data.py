3fb5df714dcb898ec41cc1413246e624400e9693
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-16 12:58:54 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -14,13 +14,11 @@ CUMA TEST DOANG
 
     # Rest Buttons
     buttons = [
-        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/StarkBots/7")],
+        [InlineKeyboardButton("✨ UPDATE CHANNEL ✨", url="https://t.me/StarkBots/7")],
         [
-            InlineKeyboardButton("How to Use ❔", callback_data="help"),
-            InlineKeyboardButton("🎪 About 🎪", callback_data="about")
+            InlineKeyboardButton("RATING", callback_data="help"),
+            InlineKeyboardButton("DONASI", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
-        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/StarkBotsChat")],
     ]
 
     # Help Message
