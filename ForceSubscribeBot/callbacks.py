0132637aade678c066301fe448cb7bb678963d2c
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-18 21:17:46 +0700

    Update callbacks.py

--- ForceSubscribeBot/callbacks.py
+++ ForceSubscribeBot/callbacks.py
@@ -1,6 +1,6 @@
 from Data import Data
 from pyrogram import Client
-from pyrogram.types import CallbackQuery, InlineKeyboardMarkup
+from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, Message
 from pyrogram.errors.exceptions import UserNotParticipant
 from ForceSubscribeBot.database.chats_sql import (
     get_action,
@@ -35,10 +35,9 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
     elif query == "donasi":
         chat_id = callback_query.from_user.id
         message_id = callback_query.message.message_id
-        await bot.edit_message_text(
-            chat_id=chat_id,
-            message_id=message_id,
-            text="DONASI",
+        await message.reply_photo(
+		    "https://telegra.ph/file/591df41289c406ed4f248.jpg",
+		    caption="DONASI"
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
