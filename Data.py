6bb6f4fb89b3aa3b31a9fbb1fbbc4040a7dc8f27
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-18 16:29:40 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -6,7 +6,7 @@ class Data:
    
     # Home Button
     start_buttons = [
-        [InlineKeyboardButton(text="🏠 KEMBALI 🏠", /start")],
+        [InlineKeyboardButton(text="🏠 KEMBALI 🏠", "/start")],
     ]
 
     # Rest Buttons
@@ -14,9 +14,9 @@ class Data:
         [InlineKeyboardButton("✨ SUPPORT CHANNEL ✨", url="https://t.me/NekoPoiSupport")],
         [
             InlineKeyboardButton("RATING", url="https://t.me/NekopoiSupport/9"),
-            InlineKeyboardButton("DONASI", callback_data="donasi")
+            InlineKeyboardButton("DONASI", "/donasi")
         ],
-        [InlineKeyboardButton("❗ LAPOR LINK RUSAK ❗", callback_data="lapor")]
+        [InlineKeyboardButton("❗ LAPOR LINK RUSAK ❗", "/lapor")]
     ]
 
     # Help Message
