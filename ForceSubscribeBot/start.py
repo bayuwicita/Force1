78375480a244eac73dd2f0b3c97806cf291a2127
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-18 10:28:22 +0700

    Update start.py

--- ForceSubscribeBot/start.py
+++ ForceSubscribeBot/start.py
@@ -11,6 +11,5 @@ async def start(bot, msg, client : Client, message : Message):
 	await bot.send_photo(
 		msg.chat.id,
 		photo = "https://telegra.ph/file/ef7261e2a4bec533ec771.jpg"
-		caption = "SOme Caption"
 		reply_markup=InlineKeyboardMarkup(Data.buttons)
 	)
