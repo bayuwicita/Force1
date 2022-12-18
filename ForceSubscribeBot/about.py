57a1c086b7968a6b8554b6f0f6fa23db0c36b8e1
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-18 15:11:20 +0700

    Update about.py

--- ForceSubscribeBot/about.py
+++ ForceSubscribeBot/about.py
@@ -4,11 +4,11 @@ from pyrogram.types import InlineKeyboardMarkup
 
 
 # About Message
-@Client.on_message(filters.private & filters.incoming & filters.command("about"))
-async def about(bot, msg):
-    await bot.send_message(
-        msg.chat.id,
-        Data.ABOUT,
-        disable_web_page_preview=True,
+@Client.on_message(filters.private & filters.incoming & filters.command("donasi"))
+async def donasi(client: Client, message: Message):
+	await message.reply_photo(
+		"https://telegra.ph/file/591df41289c406ed4f248.jpg",
+		caption="**Jika anda menyukai Bot kami dan ingin memberikan donasi serta dukungan kepada kami bisa dengan cara scan gambar kode QRIS diatas lalu input nilai yang ingin ditransfer.**",
+		disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons),
-    )
+	)
