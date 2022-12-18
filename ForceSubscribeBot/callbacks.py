64752a32b2124b9d6ec3c3abceba0a9128dd0dcf
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-18 23:04:52 +0700

    Update callbacks.py

--- ForceSubscribeBot/callbacks.py
+++ ForceSubscribeBot/callbacks.py
@@ -33,15 +33,15 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
                 reply_markup=InlineKeyboardMarkup(Data.buttons),
             )
             
-            elif query == 'donasi':
-                kode_qr = InputMediaPhoto(
-                    media="https://telegra.ph/file/591df41289c406ed4f248.jpg",
-                    caption="TEST"
-                )
-                await callback.edit_message_media(
-                    media=kode_qr,
-                    reply_markup=InlineKeyboardMarkup(Data.home_buttons)
-                )
+    elif query == 'donasi':
+         kode_qr = InputMediaPhoto(
+         media="https://telegra.ph/file/591df41289c406ed4f248.jpg",
+         caption="TEST"
+         )
+          await callback.edit_message_media(
+          media=kode_qr,
+          reply_markup=InlineKeyboardMarkup(Data.home_buttons)
+         )
     elif query == "help":
         chat_id = callback_query.from_user.id
         message_id = callback_query.message.message_id
