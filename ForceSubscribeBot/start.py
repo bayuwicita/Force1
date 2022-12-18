526f0de4719a9663a1c7521a4001aa7384143c2b
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-18 21:43:38 +0700

    Update start.py

--- ForceSubscribeBot/start.py
+++ ForceSubscribeBot/start.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 @Client.on_message(filters.command("start"))
 async def start(client: Client, message: Message):
 	await message.reply_photo(
-		"https://telegra.ph/file/ef7261e2a4bec533ec771.jpg",
-		caption=f"**Halo : {message.from_user.mention} \nSelamat datang di NekoPoi Bot. \n\nFitur Bot:** \n➥ __No Iklan.__\n➥ __Akses Sangat Mudah.__\n➥ __Bebas Streaming & Download.__\n\n**Enjoy !**",
+		"https://telegra.ph/file/1b04eb25d83788b169f73.jpg",
+		caption=f"**Halo : {message.from_user.mention} \nSelamat datang di Anime Bot. \n\nFitur Bot:** \n➥ __No Iklan.__\n➥ __Akses Sangat Mudah.__\n➥ __Bebas Streaming & Download.__\n\n**Enjoy !**",
 		reply_markup=InlineKeyboardMarkup(Data.buttons)
 	)
