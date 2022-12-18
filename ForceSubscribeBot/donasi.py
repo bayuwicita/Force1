239716b046139bfb9b4dc08b4c71cc788eae3890
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-18 17:32:22 +0700

    Create donasi.py

---
+++ ForceSubscribeBot/donasi.py
@@ -0,0 +1,13 @@
+from Data import Data
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+
+# Start Message
+@Client.on_message(filters.command("donasi"))
+async def donasi(client: Client, message: Message):
+	await message.reply_photo(
+		"https://telegra.ph/file/591df41289c406ed4f248.jpg",
+		caption=**Jika anda menyukai Bot kami dan ingin memberikan donasi serta dukungan kepada kami bisa dengan cara scan gambar kode QRIS diatas lalu input nilai yang ingin ditransfer.**",
+		reply_markup=InlineKeyboardMarkup(Data.buttons)
+	)
