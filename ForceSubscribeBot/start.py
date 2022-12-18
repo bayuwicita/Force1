e8f6d0f1b950dcd9e17edb1b396de5ebeea50d26
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-18 13:52:55 +0700

    Update start.py

--- ForceSubscribeBot/start.py
+++ ForceSubscribeBot/start.py
@@ -6,8 +6,8 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 # Start Message
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(bot, msg):
-	await bot.send_photo(
-		"msg.chat.id",
-		"https://telegra.ph/file/591df41289c406ed4f248.jpg", 
-		caption="CaptionNEW"
+	await bot.send_message(
+		msg.chat.id,
+		Data.START,
+		reply_markup=InlineKeyboardMarkup(Data.buttons)
 	)
