dc295b2a6fa46148ceeaa431912103b5dcd45700
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-18 15:42:54 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -3,17 +3,7 @@ from os import getenv
 from pyrogram.types import InlineKeyboardButton
 
 class Data:
-    # Start Message
-    START = """
-    
-    CUMA TES AJA
-        """
-    
-    START_IMG_URL = getenv(
-        "START_IMG_URL",
-        "image/donasi.jpg"
-    )
-    
+   
     # Home Button
     home_buttons = [
         [InlineKeyboardButton(text="🏠 KEMBALI 🏠", callback_data="home")],
@@ -24,18 +14,12 @@ class Data:
         [InlineKeyboardButton("✨ SUPPORT CHANNEL ✨", url="https://t.me/NekoPoiSupport")],
         [
             InlineKeyboardButton("RATING", url="https://t.me/NekopoiSupport/9"),
-            InlineKeyboardButton("DONASI", callback_data="about")
+            InlineKeyboardButton("DONASI", callback_data="donasi")
         ],
+        [InlineKeyboardButton("❗ LAPOR LINK RUSAK ❗", url="https://t.me/NekoPoiSupport")]
     ]
 
     # Help Message
     HELP = """
 **Jika ada kendala atau ingin melaporkan link rusak bisa chat admin di [Channel NekoPoi](https://t.me/NekoPoiSupport).**
-    """
-
-    # About Message
-    ABOUT = """
-**DONASI** 
-
-Jika anda menyukai Bot kami dan ingin memberikan donasi serta dukungan kepada kami bisa dengan cara scan gambar kode QRIS diatas lalu input nilai yang ingin ditransfer.
     """
