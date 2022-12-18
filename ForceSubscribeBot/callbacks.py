613fa44d43e63232ecb54a14836bb8a5db4cf4c0
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-18 23:14:47 +0700

    Update callbacks.py

--- ForceSubscribeBot/callbacks.py
+++ ForceSubscribeBot/callbacks.py
@@ -34,6 +34,8 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             )
             
     elif query == 'donasi':
+         chat_id = callback_query.from_user.id
+         message_id = callback_query.message.message_id
          kode_qr = InputMediaPhoto(
          media="https://telegra.ph/file/591df41289c406ed4f248.jpg",
          caption="TEST"
