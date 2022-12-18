7b86bbb70593f3dced56aeb56291f7eb9d1a33a3
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-18 13:02:21 +0700

    Update start.py

--- ForceSubscribeBot/start.py
+++ ForceSubscribeBot/start.py
@@ -1,6 +1,6 @@
 from Data import Data
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardMarkup, Message
 
 
 # Start Message
